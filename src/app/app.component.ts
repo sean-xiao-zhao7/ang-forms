7ecb845cc28d550b7c6b90b24d340a565af725a8
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-12-07 14:51:14 -0500

    Use FormArray

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -13,15 +13,25 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.form = new FormGroup({
-      username: new FormControl(null),
-      email: new FormControl(null),
-      secret: new FormControl(null),
-      answer: new FormControl(null),
-      gender: new FormControl(this.genders[0]),
+      personalGroup: new FormGroup({
+        username: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      secret: new FormControl(null, Validators.required),
+      answer: new FormControl(null, Validators.required),
+      gender: new FormControl(this.genders[0], Validators.required),
+      hobbies: new FormArray([]),
     });
   }
 
   suggestUserName() {}
 
-  onSubmit() {}
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.form.get('hobbies')).push(control);
+  }
+
+  onSubmit() {
+    console.log(this.form);
+  }
 }
